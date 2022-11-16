918dfb98caf0def065affa98a96663c010ad41b5
farhank-0 <farhankassam0@gmail.com>
2022-11-16 14:24:07 -0500

    Color mode, grid size change, grid element style
    Added javascript to change colors of div elements based on color picker.
    Added slider to change grid size. Added button to clear and reload grid.
    
    Commit: [2]

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 const grid = document.getElementById('grid');
+const colourPicker = document.getElementById('colourPicker');
+const colourBtn = document.getElementById('colourBtn');
+const randomBtn = document.getElementById('randomBtn');
+const eraserBtn = document.getElementById('eraserBtn');
+const clearBtn = document.getElementById('clearBtn');
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById('sizeSlider');
 
-setupGrid(16);
+colourPicker.oninput = (e) => setColour(e.target.value);
+colourBtn.onclick = () => setMode('colour');
+randomBtn.onclick = () => setMode('random');
+eraserBtn.onclick = () => setMode('eraser');
+clearBtn.onclick = () => reloadGrid();
+sizeValue.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+
+let mode = 'colour';
+let size = 16;
+let colour = '#000000';
+let mouseDown = false;
+
+function setMode(newMode) {
+    mode = newMode;
+}
+
+function setColour(newColour) {
+    colour = newColour;
+}
+
+function updateSizeValue(size) {
+    sizeValue.textContent = `${size} X ${size}`;
+}
+
+function changeSize(newSize) {
+    size = newSize;
+    reloadGrid();
+    updateSizeValue(newSize);
+}
+
+function reloadGrid() {
+    grid.textContent = '';
+    setupGrid(size);
+}
 
 function setupGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -9,6 +51,15 @@ function setupGrid(size) {
     for(let i=0; i< size*size; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
+        gridElement.addEventListener("mousedown", () => mouseDown = true);
+        gridElement.addEventListener("mouseup", () => mouseDown = false);
+        gridElement.addEventListener("mousemove", changeColour);
         grid.appendChild(gridElement);
+    }
+}
+
+function changeColour(e) {
+    if (mouseDown && e.type === 'mousemove'){
+        e.target.style.backgroundColor = colour;
     }
 }
